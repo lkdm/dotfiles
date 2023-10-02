136a97f779d236bb6ef6d96089f0a010398924e5
Luke M <44519853+lkdm@users.noreply.github.com>
2023-10-03 09:33:25 +1100

    feat: create personal and work brewfile.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,14 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
-brew bundle --file ~/.dotfiles/Brewfile
 
 # Dotfile symlinks
 ln -fs ~/.dotfiles/.zshrc ~/.zshrc
 ln -fs ~/.dotfiles/.gitconfig ~/.gitconfig
+
+echo -e "# Choose which brewfile to run: \n\
+# Personal Brewfile \n\
+\t brew bundle --file ~/.dotfiles/brewfile/personal \n\
+\n\
+# Work Brewfile \n\
+\t brew bundle --file ~/.dotfiles/brewfile/work"
