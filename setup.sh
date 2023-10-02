379e6809e016a7888c6d1a4243f7cf81f97594f3
Luke M <44519853+lkdm@users.noreply.github.com>
2023-10-03 09:15:58 +1100

    fix: double up on git clone.

--- setup.sh
+++ setup.sh
@@ -5,11 +5,6 @@
 brew update
 brew bundle --file ~/.dotfiles/Brewfile
 
-# Deploys dotfiles to home directory
-cd ~/
-git clone git@github.com:lkdm/dotfiles.git
-mv dotfiles .dotfiles
-
 # Dotfile symlinks
 ln -fs ~/.dotfiles/.zshrc ~/.zshrc
 ln -fs ~/.dotfiles/.gitconfig ~/.gitconfig
