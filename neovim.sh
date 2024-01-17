549d1998263d021888df51dd59e127b6918125df
Luke M <44519853+lkdm@users.noreply.github.com>
2024-01-18 07:34:37 +1100

    feat: add neovim config.

---
+++ neovim.sh
@@ -0,0 +1,8 @@
+# Installs Neovim and its dependencies
+
+brew install neovim
+
+# Install vimplug
+# https://github.com/junegunn/vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
