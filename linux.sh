70a0ed2b78fabbfa9ab3f35cfad51bfc2372f7c1
Luke M <44519853+lkdm@users.noreply.github.com>
2025-01-16 15:04:38 +1100

    feat: add mt script.

--- linux.sh
+++ linux.sh
@@ -12,3 +12,6 @@ echo "source $(pwd)/sh.sh" >> ~/.bashrc
 # Set up a script to automatically open your dev setup in regolith
 sudo ln -s /home/luke/github.com/lkdm/dotfiles/scripts/i3.sh /usr/local/bin/i3-start
 echo -e "\ni3-wm.program.1: /usr/local/bin/i3-start" >> ~/.config/regolith3/Xresources
+
+# Set up mt cli tool
+sudo ln -s /home/luke/github.com/lkdm/dotfiles/scripts/mt.sh /usr/local/bin/mt
