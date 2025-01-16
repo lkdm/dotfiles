d54b29ec9d56cb4e0747fae2a82c945668c5e8de
Luke M <44519853+lkdm@users.noreply.github.com>
2025-01-16 15:50:20 +1100

    feat: make i3-start use mt tool instead of sh functions.

--- scripts/i3-start.sh
+++ scripts/i3-start.sh
@@ -7,10 +7,7 @@ sleep 1 && i3-msg 'workspace 1'
 sleep 1.5 && i3-msg 'exec alacritty'
 sleep 0.2 && i3-msg 'workspace 1; [class="Alacritty"] focus'
 # Execute commands in the focused Alacritty window
-sleep 0.5 && i3-msg 'exec --no-startup-id xdotool type "mup" && xdotool key Return'
-sleep 2 && i3-msg 'exec --no-startup-id xdotool key ctrl+c'
-sleep 0.25 && i3-msg 'exec --no-startup-id xdotool type "msh" && xdotool key Return'
-sleep 0.5 && i3-msg 'exec --no-startup-id xdotool type "start" && xdotool key Return'
+sleep 0.5 && i3-msg 'exec --no-startup-id xdotool type "mt start" && xdotool key Return'
 
 sleep 0.5 && i3-msg 'split v, exec alacritty'
 sleep 0.5 && i3-msg 'split v, exec alacritty'
