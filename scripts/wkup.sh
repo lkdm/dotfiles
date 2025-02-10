31d1fa7ab38c348693080fd8706d1f288f00ac71
Luke M <44519853+lkdm@users.noreply.github.com>
2025-02-10 11:24:41 +1100

    feat: add welcome message to wkup.

--- scripts/wkup.sh
+++ scripts/wkup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Start Tmux
+echo "Welcome! Starting your work environment..."
 tmux -d
 sleep 0.5 && i3-msg 'split h, exec firefox'
 sleep 1 && i3-msg '[class="firefox"] resize shrink width 50 px'
@@ -8,3 +9,4 @@ sleep 0.5 && i3-msg 'split h, exec obsidian'
 sleep 1.5 && i3-msg '[class="obsidian"] resize shrink width 200 px'
 sleep 1 && i3-msg '[class="firefox"] focus'
 sleep 1 && tmux attach
+echo "Have a great rest of the day!"
