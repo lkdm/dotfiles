3cb18e8b23aa2435f8a99e31278393efc74ee6ba
Luke M <44519853+lkdm@users.noreply.github.com>
2025-02-11 12:35:59 +1100

    feat: update chezmoi.

--- osx.sh
+++
@@ -1,6 +0,0 @@
-# Finder: Show hidden files
-defaults write com.apple.Finder AppleShowAllFiles true
-killall Finder
-
-# Disable press and hold for keys in VSCode
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
