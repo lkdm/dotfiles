94dd24f3161040dc0c80ce3216b010fbc0a4ce14
Luke M <44519853+lkdm@users.noreply.github.com>
2025-02-11 14:45:22 +1100

    feat: get executables working correctly.

--- home/run_install_user_scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.local/bin
-find ~/github.com/lkdm/dotfiles/bin -type f \( -name "*.sh" -o -name "*.py" -o -name "*.rs" -o -name "*.ts" \) \
-  -exec sh -c '
-    source_file="{}"
-    target_name="$(basename "{}" | sed "s/\.[^.]*$//")"
-    ln -sf "$source_file" ~/.local/bin/"$target_name"
-  ' \;
-
