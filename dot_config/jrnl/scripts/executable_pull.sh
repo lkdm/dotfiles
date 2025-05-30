d27fa7e5f47d227e9e4d93b2114b1e39ba7deda0
Luke M <44519853+lkdm@users.noreply.github.com>
2025-05-30 11:06:03 +1000

    perf(jrnl): fetch and only pull journal entries if there are changes
    This slightly speeds up the performance of the jrnl command

--- dot_config/jrnl/scripts/executable_pull.sh
+++ dot_config/jrnl/scripts/executable_pull.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-JRNL_GIT_DIR=$JRNL
+JRNL_GIT_DIR="$JRNL"
 
 pushd "$JRNL_GIT_DIR" > /dev/null
 
-git pull --quiet
-if [ $? -ne 0 ]; then
-  printf "\033[0;31m[git pull failed]\033[0m\n" >&2
+# Fetch remote updates
+git fetch --quiet
+
+# Check if local branch is behind remote
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse @{u})
+
+if [ "$LOCAL" != "$REMOTE" ]; then
+  git pull --rebase --quiet
+  if [ $? -ne 0 ]; then
+    printf "\033[0;31m[git pull/rebase failed]\033[0m\n" >&2
+  fi
 fi
 
 popd > /dev/null
