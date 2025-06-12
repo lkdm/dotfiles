b2991ab099c47e766e48e421806bac4a4f4133ae
Luke M <44519853+lkdm@users.noreply.github.com>
2025-06-12 15:19:34 +1000

    chore(nvim): enable spellcheck

--- dot_config/nvim/lua/polish.lua
+++ dot_config/nvim/lua/polish.lua
@@ -24,6 +24,8 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.wrap = true
     vim.opt_local.linebreak = true
     vim.opt_local.textwidth = 80
+	vim.opt_local.spell = true
+	vim.opt_local.spelllang = "en_au"
   end,
 })
 
