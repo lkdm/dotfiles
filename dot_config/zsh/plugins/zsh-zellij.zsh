da18e5815f77bf4d1b9fee9745eb0c0a5bf2a511
Luke M <44519853+lkdm@users.noreply.github.com>
2025-06-13 12:13:19 +1000

    chore(zellij): rename pane improvements

--- dot_config/zsh/plugins/zsh-zellij.zsh
+++ dot_config/zsh/plugins/zsh-zellij.zsh
@@ -1,26 +1,35 @@
+
+function _zellij_pane_pwd() {
+  if [[ -n $ZELLIJ ]]; then
+    # local current_dir=$(pwd)
+    # local home_dir=$HOME
+    # # Resolve symlinks for robust comparison
+    # current_dir=$(realpath -- "$current_dir" 2>/dev/null || echo "$current_dir")
+    # home_dir=$(realpath -- "$home_dir" 2>/dev/null || echo "$home_dir")
+    # # Remove trailing slashes
+    # current_dir=${current_dir%/}
+    # home_dir=${home_dir%/}
+    # if [[ "$current_dir" == "$home_dir" ]]; then
+    #   current_dir="~"
+    # else
+    #   # Ensure replacement works for subdirectories
+    #   if [[ "$current_dir" == "$home_dir"/* ]]; then
+    #     current_dir="~${current_dir#$home_dir}"
+    #   fi
+    # fi
+    command nohup zellij action rename-pane "shell" >/dev/null 2>&1
+  fi
+}
+
+
+chpwd_functions=(${chpwd_functions[@]} "_zellij_pane_pwd")
+precmd_functions=(${precmd_functions[@]} "_zellij_pane_pwd")
+
+# When running a foreground process, rename to its resolved command
 function _zellij_pane_app(){
     if [[ -n $ZELLIJ ]]; then 
       local name=$(echo $3 | cut -d' ' -f1)
       command nohup zellij action rename-pane $name >/dev/null 2>&1
     fi
 }
-
-# TODO remember to credit algusdark for this inspiration
-# https://www.reddit.com/r/zellij/comments/10skez0/does_zellij_support_changing_tabs_name_according/
-function _zellij_pane_pwd(){
-    if [[ -n $ZELLIJ ]]; then 
-      local current_dir=$PWD 
-      if [[ $current_dir == $HOME ]]; then 
-        current_dir="~" 
-      else 
-        current_dir=${current_dir##*/} 
-      fi 
-        command nohup zellij action rename-pane $current_dir >/dev/null 2>&1 
-    fi
-}
-
-chpwd_functions=(${chpwd_functions[@]} "_zellij_pane_pwd")
 preexec_functions=(${preexec_functions[@]} "_zellij_pane_app")
-precmd_functions=(${precmd_functions[@]} "_zellij_pane_ls_pwd")
-
-
