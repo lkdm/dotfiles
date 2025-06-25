5d2033ab6d3e22c9945c38a13f95f2ccc0ba2c54
Luke M <44519853+lkdm@users.noreply.github.com>
2025-06-25 10:13:46 +1000

    chore(nvim): make nvim-purge more descriptive.

--- bin/executable_nvim-purge.sh
+++ bin/executable_nvim-purge.sh
@@ -3,7 +3,7 @@
 # @about: Purge nvim config and plugins
 # @usage: nvim-purge
 echo "Purging nvim config and plugins..."
-rm ~/.config/nvim -rf
-rm ~/.local/share/nvim/lazy -rf
-rm ~/.local/share/nvim/mazon -rf
-echo "Run `chezmoi apply` to repopulate nvim configuration"
+rm ~/.config/nvim -rf && echo "Purged config"
+rm ~/.local/share/nvim/lazy -rf && echo "Purged lazy packages"
+rm ~/.local/share/nvim/mazon -rf && echo "Purged mason packages"
+echo "Run \`chezmoi apply\` to repopulate nvim configuration"
